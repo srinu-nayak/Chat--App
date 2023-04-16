e1aeff9d3207db0dd553d92b304386528addef72
SRINU-NAYAK <94540638+SRINU-NAYAK@users.noreply.github.com>
2023-04-16 21:01:25 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -12,15 +12,17 @@ import 'firebase/auth';
 //   appId: "1:796315991506:web:3dcc0a3db152b4987e5c0b",
 //   measurementId: "G-CP3SRVL9R4"
 //   })
-const firebaseConfig = {
-  apiKey: "AIzaSyDny5hbTs3_R4SxiNOSCvFK2Xh6pk6KfQ0",
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDny5hbTs3_R4SxiNOSCvFK2Xh6pk6KfQ0",
   authDomain: "chat-app-e3920.firebaseapp.com",
   projectId: "chat-app-e3920",
   storageBucket: "chat-app-e3920.appspot.com",
   messagingSenderId: "818812141719",
   appId: "1:818812141719:web:032d4372c415c7ffb929d2",
   measurementId: "G-GF8PZVGKX9"
-};
+  })
+
 
 
 const auth = firebase.auth();
